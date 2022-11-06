583ed38d616521e07b275c494814a966217f43e2
Leandro Gehlen <legedeveloper@gmail.com>
2022-11-05 23:05:56 -0300

    creating the controller

--- src/D-presentation-layer/controllers/lastrankingloadercontroller.ts
+++ src/D-presentation-layer/controllers/lastrankingloadercontroller.ts
@@ -2,7 +2,7 @@ import { Controller, HttpResponse } from "../contracts";
 import { RankingScoreViewModel } from "../representation";
 
 export class LastRankingLoarderController implements Controller {
-    async handle(): Promise<HttpResponse<RankingScoreViewModel>> {
+    async handle(): Promise<HttpResponse<RankingScoreViewModel[]>> {
 
     }
 }
